ece1d34c54772ff9a4dc56f29f408efbf6909eb6
OHASHI Hideya <ohachige@gmail.com>
2013-12-30 10:02:27 +0900

    change to sakuracoin (qt)

--- src/qt/aboutdialog.cpp
+++ src/qt/aboutdialog.cpp
@@ -15,7 +15,8 @@ AboutDialog::AboutDialog(QWidget *parent) :
     ui->setupUi(this);
 
     // Set current copyright year
-    ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; 2009-%1 ").arg(COPYRIGHT_YEAR) + tr("The Bitcoin developers") + QString("<br>") + tr("Copyright") + QString(" &copy; ") + tr("2011-%1 The Litecoin developers").arg(ABOUTDIALOG_COPYRIGHT_YEAR));
+    ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; 2009-%1 ").arg(COPYRIGHT_YEAR) + tr("The Bitcoin developers") + QString("<br>") + tr("Copyright") + QString(" &copy; ") + tr("2011-%1 The Litecoin developers").arg(ABOUTDIALOG_COPYRIGHT_YEAR) +
+      tr("2013-%1 The Sakuracoin developers").arg(ABOUTDIALOG_COPYRIGHT_YEAR));
 }
 
 void AboutDialog::setModel(ClientModel *model)
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Sakuracoins");
+    case mBTC: return QString("Milli-Sakuracoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Sakuracoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
