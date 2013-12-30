0d7f77133f5be3e3ca626f563f226f19100d60ce
OHASHI Hideya <ohachige@gmail.com>
2013-12-30 10:21:59 +0900

    LTC to SKR

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("SKR");
+    case mBTC: return QString("mSKR");
+    case uBTC: return QString::fromUtf8("μSKR");
     default: return QString("???");
     }
 }
